923852d6411f1775dab17e746038fee41b4a718a
k0n <artk0n@hotmail.com>
2025-09-10 01:04:31 +1000

    lol

--- Models/DockData.cs
+++ Models/DockData.cs
@@ -1,19 +1,9 @@
-using System;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 
 namespace DockTop.Models
 {
-    public class DockItem
-    {
-        public string Title { get; set; } = "";
-        public string Path  { get; set; } = "";
-        public bool   Disabled { get; set; } = false;
-        public bool   AddSlot  { get; set; } = false;
-    }
-
-    // ultra-minimal in-memory store (no persistence needed for compile)
+    // Central in-memory item list used by the UI.
     public static class Items
     {
         public static ObservableCollection<DockItem> All { get; } = new ObservableCollection<DockItem>();
@@ -21,13 +11,14 @@ namespace DockTop.Models
         public static void AddFromPath(string path)
         {
             if (string.IsNullOrWhiteSpace(path)) return;
-            All.Add(new DockItem {
-                Title = System.IO.Path.GetFileNameWithoutExtension(path),
+            All.Add(new DockItem
+            {
+                Title = Path.GetFileNameWithoutExtension(path),
                 Path  = path
             });
         }
 
-        public static void Save() { /* no-op for now */ }
-        public static void Load() { /* no-op for now */ }
+        public static void Save() { /* no-op (todo: persist to JSON) */ }
+        public static void Load() { /* no-op */ }
     }
 }
